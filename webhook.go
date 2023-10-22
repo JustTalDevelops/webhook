eb00d689c2d7ed47d664e2480908a85c69fc788e
JustTalDevelops <justtal@protonmail.com>
2023-10-22 16:16:14 -0500

    webhook.go: Omit empty most fields.

--- webhook.go
+++ webhook.go
@@ -2,31 +2,31 @@ package webhook
 
 // Webhook is the webhook object sent to Discord in hook requests.
 type Webhook struct {
-	Username  string  `json:"username"`
-	AvatarURL string  `json:"avatar_url"`
-	Content   string  `json:"content"`
-	Embeds    []Embed `json:"embeds"`
+	Username  string  `json:"username,omitempty"`
+	AvatarURL string  `json:"avatar_url,omitempty"`
+	Content   string  `json:"content,omitempty"`
+	Embeds    []Embed `json:"embeds,omitempty"`
 }
 
 // Embed is the embed object in the webhook.
 type Embed struct {
-	Author      Author  `json:"author"`
-	Footer      Footer  `json:"footer"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Timestamp   string  `json:"timestamp"`
-	Thumbnail   Image   `json:"thumbnail"`
-	Image       Image   `json:"image"`
-	URL         string  `json:"url"`
-	Fields      []Field `json:"fields"`
-	Color       int64   `json:"color"`
+	Author      Author  `json:"author,omitempty"`
+	Footer      Footer  `json:"footer,omitempty"`
+	Title       string  `json:"title,omitempty"`
+	Description string  `json:"description,omitempty"`
+	Timestamp   string  `json:"timestamp,omitempty"`
+	Thumbnail   Image   `json:"thumbnail,omitempty"`
+	Image       Image   `json:"image,omitempty"`
+	URL         string  `json:"url,omitempty"`
+	Fields      []Field `json:"fields,omitempty"`
+	Color       int64   `json:"color,omitempty"`
 }
 
 // Author is the author object in the embed.
 type Author struct {
-	Name    string `json:"name"`
-	URI     string `json:"uri"`
-	IconURL string `json:"icon_url"`
+	Name    string `json:"name,omitempty"`
+	URI     string `json:"uri,omitempty"`
+	IconURL string `json:"icon_url,omitempty"`
 }
 
 // Field is a field in the embed.
@@ -38,8 +38,8 @@ type Field struct {
 
 // Footer is the footer object in the embed.
 type Footer struct {
-	Text    string `json:"text"`
-	IconURL string `json:"icon_url"`
+	Text    string `json:"text,omitempty"`
+	IconURL string `json:"icon_url,omitempty"`
 }
 
 // Image is an image that can be used in the embed.
